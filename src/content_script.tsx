2933ca4bba1f0204cf75cc69d29bd1323ba8ed1f
Andy Zhang <andzhng@gmail.com>
2023-08-29 17:45:01 -0700

    css module working

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,4 +1,17 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import classes from "./content_script.module.css";
+
+function App() {
+  console.log(classes);
+  return (
+    <>
+      <h1 className={classes.bg}>Hello World</h1>
+    </>
+  )
+}
+
+chrome.runtime.onMessage.addListener(function (msg: any, sender: chrome.runtime.MessageSender, sendResponse: Function) {
   if (msg.color) {
     console.log("Hey hey ");
     console.log("Receive color = " + msg.color);
@@ -8,3 +21,24 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse("Color message is none.");
   }
 });
+
+
+function init() {
+  const body = document.querySelector('body')
+  const app = document.createElement('div')
+
+  app.id = 'react-root'
+
+  console.log("RENDER!");
+  if (body) {
+    body.prepend(app)
+  }
+
+  const container = document.getElementById('react-root');
+  const root = createRoot(container!);
+
+  root.render(<App/>)  // Render react component
+}
+
+
+window.addEventListener('load', init);
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
@@ -29,15 +30,35 @@ module.exports = {
                 use: "ts-loader",
                 exclude: /node_modules/,
             },
+            {
+              test: /\.css$/,
+              use: [
+                "style-loader",
+                {
+                  loader: "css-loader",
+                  options: {
+                    importLoaders: 1,
+                    modules: true,
+                  },
+                },
+              ],
+              include: /\.module\.css$/,
+            },
+            {
+              test: /\.css$/,
+              use: ["style-loader", "css-loader"],
+              exclude: /\.module\.css$/,
+            },
         ],
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
     },
     plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+      new CopyPlugin({
+          patterns: [{ from: ".", to: "../", context: "public" }],
+          options: {},
+      }),
+      new MiniCssExtractPlugin()
     ],
 };
