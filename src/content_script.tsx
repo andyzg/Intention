7f70a7bbee115a987c275eae7e7a70be58eb27d3
Andy Zhang <andzhng@gmail.com>
2023-08-29 17:11:49 -0700

    working content script

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
+    console.log("Hey hey ");
     console.log("Receive color = " + msg.color);
     document.body.style.backgroundColor = msg.color;
     sendResponse("Change color to " + msg.color);
